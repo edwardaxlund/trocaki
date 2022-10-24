415703ff73d3fd8e8cbd173f3f137f5602cf28d0
edwardaxlund <55448516+edwardaxlund@users.noreply.github.com>
2022-10-24 20:18:49 +0200

    navbar done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,48 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 import './navbar.css';
-import  logo from '../../assets/logo.png'
+import  logo from '../../assets/Png/Logotype-black.png'
+
+const Menu = () =>{
+    <>
+    <p><a href="#home" >Home</a></p>
+    <p><a href="#trocaki" >What is Trocaki</a></p>
+    <p><a href="#features" >Get involved</a></p>
+    <p><a href="#blog" >Links</a></p>
+    </>
+} 
+
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+
     return (
-        <div className = "trocaki_navbar">
-            <div className = 'trocaki_navbar-links'>
-                <div className = 'trocaki_navbar-links_logo'>
-                    <img src = {logo} alt = "logo"/>
+        <div className = "trocaki__navbar">
+            <div className = 'trocaki__navbar-links'>
+                <div className = 'trocaki__navbar-links_logo'>
+                    <img src = {logo} alt = "logo" />                    
                 </div>
+                <div className = "trocaki__navbar-links_container">
+                    <p><a href="#home" >Home</a></p>
+                    <p><a href="#trocaki" >What is Trocaki</a></p>
+                    <p><a href="#features" >Get involved</a></p>
+                    <p><a href="#blog" >Links</a></p>
+                </div>
+            </div>
+            <div className = "trocaki__navbar-sign">
+            <button type ="button">Sign up</button>
+            </div>
+            <div className = "trocaki__navbar-menu">
+                {toggleMenu
+                ? <RiCloseLine color="#ffff" size = {27} onClick= {() => setToggleMenu(false)}/>
+                : <RiMenu3Line color="#ffff" size = {27} onClick= {() => setToggleMenu(true)}/>
+                }
+                {toggleMenu && (
+                <div className = "trocaki__navbar-menu_container scale-up-center">
+                    <div className = "trocaki__navbar-menu_container-links">
+                        <Menu/>
+                    </div>
+                </div>
+                )}
             </div>
         </div>
     )
