025607eb5176b42e40f84901bc13565c9f5a40f6
edwardaxlund <55448516+edwardaxlund@users.noreply.github.com>
2022-10-25 15:44:54 +0200

    Home page almost done

--- src/containers/headers/Headers.jsx
+++ src/containers/headers/Headers.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import './headers.css';
+import bird from "../../assets/Png/Logomark-Black.png"
 
 const Headers = () => {
     return (
-        <div>
-            Headers
+        <div className = "trocaki__header section_padding" id = "home">
+            <div className = "trocaki__header-content">
+                <h1 className = "gradient__text">
+                    Join Trocaki, bringing crypto to Brazil
+                </h1>
+                <p>Join us and enjoy easy crypto exchange</p>
+                <div className = "trocaki__header-content__input">
+                    <input type = "email" placeholder = "Email"></input>
+                    <button type ="button" >Get started</button>
+                </div>
+
+                <div className = "trocaki__header-image">
+                    <img src = {bird}/>
+                </div>
+            </div>
         </div>
     )
 }
